bb63aef6f702398ed52f4912b76a7a10d18032e7
Arcturus917 <151203395+Arcturus917@users.noreply.github.com>
2024-11-10 11:29:01 -0500

    Update Sit.lua

--- Sit.lua
+++ Sit.lua
@@ -1,11 +1,5 @@
--- This script makes the player sit when it's run
-
--- Get the player's character
-local player = game.Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-
--- Wait for the Humanoid to load
-local humanoid = character:WaitForChild("Humanoid")
-
--- Make the character sit
-humanoid.Sit = true
+local human = kevyn.Parent:findFirstChild("Humanoid")
+if (human ~= nil)then
+human.Sit=true
+end
+end
